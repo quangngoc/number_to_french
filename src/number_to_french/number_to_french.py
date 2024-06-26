339eeee7fac363fbf151a3fb62fc1b4478775dc1
quangngoc <quangngoc88@gmail.com>
2024-06-26 20:54:54 +0200

    Number to french converter (< million)

--- src/number_to_french/number_to_french.py
+++ src/number_to_french/number_to_french.py
@@ -27,12 +27,69 @@ french_words = [
 numbers = list(range(17)) + [20, 30, 40, 50, 60]
 number_to_french_dict = {number: word for number, word in zip(numbers, french_words)}
 
+
 def less_than_20_to_french(tens: int, remainder: int) -> Iterator[str]:
+    """Convert numbers less than 20 into French words."""
     if tens > 1:
-        raise ValueError("Number too large")
+        # Won't handle the case where number > 19
+        return
     elif tens == 1 and remainder > 6:
+        # Numbers in [10, 17, 18, 19].
         yield "dix"
         if remainder > 0:
             yield number_to_french_dict[remainder]
     elif tens != 0 or remainder != 0:
+        # Numbers between 1 and 9.
         yield number_to_french_dict[tens * 10 + remainder]
+
+
+def number_to_french(number: int) -> Iterator[str]:
+    """Convert a number to French."""
+    if number >= 10**6:
+        raise ValueError("Number too large")
+    if number == 0:
+        yield number_to_french_dict[number]
+        return
+
+    # Handle thousands.
+    (thousands, remainder) = divmod(number, 1000)
+    if thousands > 0:
+        if thousands > 1:
+            yield from number_to_french(thousands)
+        if thousands > 1 and remainder == 0:
+            yield "milles"
+        else:
+            yield "mille"
+        if remainder == 1:
+            yield "et"
+
+    # Handle hundreds.
+    (hundreds, remainder) = divmod(remainder, 100)
+    if hundreds > 0:
+        if hundreds > 1:
+            yield number_to_french_dict[hundreds]
+        if hundreds > 1 and remainder == 0:
+            yield "cents"
+        else:
+            yield "cent"
+        if remainder == 1:
+            yield "et"
+
+    # Handle less than 100.
+    (tens, remainder) = divmod(remainder, 10)
+    if tens > 7:
+        yield "quatre-vingt"
+        yield from less_than_20_to_french(tens - 8, remainder)
+    elif tens > 5:
+        yield "soixante"
+        if remainder == 1:
+            yield "et"
+        yield from less_than_20_to_french(tens - 6, remainder)
+    elif tens > 1:
+        yield number_to_french_dict[tens * 10]
+        if remainder == 1:
+            yield "et"
+        if remainder > 0:
+            yield number_to_french_dict[remainder]
+    else:
+        yield from less_than_20_to_french(tens, remainder)
--- tests/test_number_to_french.py
+++ tests/test_number_to_french.py
@@ -1,5 +1,55 @@
 import pytest
-from number_to_french.number_to_french import less_than_20_to_french
+from number_to_french.number_to_french import less_than_20_to_french, number_to_french
+
+numbers_in_french = {
+    0: "zéro",
+    1: "un",
+    5: "cinq",
+    10: "dix",
+    11: "onze",
+    15: "quinze",
+    20: "vingt",
+    21: "vingt-et-un",
+    30: "trente",
+    35: "trente-cinq",
+    50: "cinquante",
+    51: "cinquante-et-un",
+    68: "soixante-huit",
+    70: "soixante-dix",
+    75: "soixante-quinze",
+    99: "quatre-vingt-dix-neuf",
+    100: "cent",
+    101: "cent-et-un",
+    105: "cent-cinq",
+    111: "cent-onze",
+    123: "cent-vingt-trois",
+    168: "cent-soixante-huit",
+    171: "cent-soixante-et-onze",
+    175: "cent-soixante-quinze",
+    199: "cent-quatre-vingt-dix-neuf",
+    200: "deux-cents",
+    201: "deux-cent-et-un",
+    555: "cinq-cent-cinquante-cinq",
+    999: "neuf-cent-quatre-vingt-dix-neuf",
+    1000: "mille",
+    1001: "mille-et-un",
+    1111: "mille-cent-onze",
+    1199: "mille-cent-quatre-vingt-dix-neuf",
+    1234: "mille-deux-cent-trente-quatre",
+    1999: "mille-neuf-cent-quatre-vingt-dix-neuf",
+    2000: "deux-milles",
+    2001: "deux-mille-et-un",
+    2020: "deux-mille-vingt",
+    2021: "deux-mille-vingt-et-un",
+    2345: "deux-mille-trois-cent-quarante-cinq",
+    9999: "neuf-mille-neuf-cent-quatre-vingt-dix-neuf",
+    10000: "dix-milles",
+    11111: "onze-mille-cent-onze",
+    12345: "douze-mille-trois-cent-quarante-cinq",
+    123456: "cent-vingt-trois-mille-quatre-cent-cinquante-six",
+    654321: "six-cent-cinquante-quatre-mille-trois-cent-vingt-et-un",
+    999999: "neuf-cent-quatre-vingt-dix-neuf-mille-neuf-cent-quatre-vingt-dix-neuf",
+}
 
 
 @pytest.mark.parametrize(
@@ -10,6 +60,23 @@ from number_to_french.number_to_french import less_than_20_to_french
         (1, 8, ["dix", "huit"]),
     ],
 )
-def test_less_than_20_to_french(tens, remainder, expected_output):
+def test_less_than_20_to_french(tens: int, remainder: int, expected_output: list[str]):
     result = list(less_than_20_to_french(tens, remainder))
+
+    # Assert
     assert result == expected_output
+
+
+@pytest.mark.parametrize("number", numbers_in_french.keys())
+def test_number_to_french_less_than_million(number: int):
+    result = list(number_to_french(number))
+
+    # Assert
+    words = [word for element in result for word in element.split("-")]
+    assert words == numbers_in_french[number].split("-")
+
+
+@pytest.mark.parametrize("number", [1000000, 1000001])
+def test_number_to_french_greater_than_million(number: int):
+    with pytest.raises(ValueError, match="Number too large"):
+        _ = list(number_to_french(number))
